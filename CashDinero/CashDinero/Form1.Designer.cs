1091b3e9f8d0f1686b52d2db33378180a3286ec0
SERGIO SILVA <sergio.369@hotmail.com>
2017-07-06 13:24:20 -0700

    Login Form Basic Structure

--- CashDinero/CashDinero/Form1.Designer.cs
+++ CashDinero/CashDinero/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace CashDinero
 {
-    partial class Form1
+    partial class LoginForm
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,12 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.usernameTextBox = new System.Windows.Forms.TextBox();
+            this.PasswordtextBox = new System.Windows.Forms.TextBox();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panel1.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.panel1.Controls.Add(this.loginButton);
+            this.panel1.Controls.Add(this.PasswordtextBox);
+            this.panel1.Controls.Add(this.usernameTextBox);
+            this.panel1.Location = new System.Drawing.Point(285, 137);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(495, 649);
+            this.panel1.TabIndex = 0;
+            // 
+            // usernameTextBox
+            // 
+            this.usernameTextBox.Location = new System.Drawing.Point(45, 193);
+            this.usernameTextBox.Name = "usernameTextBox";
+            this.usernameTextBox.Size = new System.Drawing.Size(414, 26);
+            this.usernameTextBox.TabIndex = 0;
+            // 
+            // PasswordtextBox
+            // 
+            this.PasswordtextBox.Location = new System.Drawing.Point(40, 311);
+            this.PasswordtextBox.Name = "PasswordtextBox";
+            this.PasswordtextBox.Size = new System.Drawing.Size(414, 26);
+            this.PasswordtextBox.TabIndex = 1;
+            // 
+            // loginButton
+            // 
+            this.loginButton.Location = new System.Drawing.Point(45, 440);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(409, 34);
+            this.loginButton.TabIndex = 2;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = true;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(1112, 1025);
+            this.Controls.Add(this.panel1);
+            this.Name = "LoginForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.TextBox PasswordtextBox;
+        private System.Windows.Forms.TextBox usernameTextBox;
     }
 }
 
